51cadea66d26c3b1910b5bd0adc36ca4dceb3fb4
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-12-13 07:15:14 +0200

    Day 13

---
+++ 2016/13/a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from collections import deque
+import fileinput
+
+
+def open(x, y):
+    F = 1364
+    #F = 10
+    n = x*x + 3*x + 2*x*y + y + y*y + F
+    return bin(n).count('1') % 2 == 0
+
+S = (1, 1)
+G = (31, 39)
+#G = (7, 4)
+seen = set([S])
+Q = deque([(0, S)])
+while Q:
+    d, p = Q.popleft()
+    for dx, dy in ((0, 1), (1, 0), (-1, 0), (0, -1)):
+        x, y = p[0] + dx, p[1] + dy
+        p2 = (x, y)
+        if x < 0 or y < 0 or p2 in seen or not open(x, y):
+            continue
+        if p2 == G:
+            print(d + 1)
+            exit(0)
+        seen.add(p2)
+        Q.append((d + 1, p2))
