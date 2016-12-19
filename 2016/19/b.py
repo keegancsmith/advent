ed5a758143d781fa43f4c09c667ab6ece38bbc99
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-12-19 12:06:01 +0200

    b with debug info

--- 2016/19/b.py
+++ 2016/19/b.py
@@ -24,12 +24,8 @@ def construct(l, r):
 
 def remove(n, i):
     assert 0 <= i < n.count
-    lc = 0
-    rc = 0
-    if n.left != None:
-        lc = n.left.count
-    if n.right != None:
-        rc = n.right.count
+    lc = 0 if n.left  is None else n.left.count
+    rc = 0 if n.right is None else n.right.count
     v = n.n
     if i < lc:
         v = remove(n.left, i)
@@ -38,14 +34,33 @@ def remove(n, i):
     n.count -= 1
     return v
 
+def print_(n, li):
+    lc = 0 if n.left is None else n.left.count
+    rc = 0 if n.right is None else n.right.count
+    if lc > 0:
+        print_(n.left, li)
+    if n.count > lc + rc:
+        li.append(n.n)
+    if rc > 0:
+        print_(n.right, li)
+
 N = 3004953
-#N = 5
+import sys
+if len(sys.argv) > 1:
+    N = int(sys.argv[1])
 root = construct(1, N)
 i = 0
 while root.count > 1:
     j = (i + root.count // 2) % root.count
+    li = []
+    print_(root, li)
+    assert root.count == len(li)
     v = remove(root, j)
-    #print(v)
+    assert v == li[j]
+    li = [('<' + x + '>') if idx == i else (('>' + x + '<') if idx == j else x) for idx, x in enumerate(map(str, li))]
+    print(' '.join(s.center(4) for s in li))
+    print(v)
     if j > i:
-        i = (i + 1) % root.count
+        i += 1
+    i = i % root.count
 print(remove(root, 0))
