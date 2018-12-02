138461a740960c1f5f1f93af846d675706071994
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-12-02 07:41:44 +0200

    2: nim solution

---
+++ 2018/2/main.nim
@@ -0,0 +1,31 @@
+import sets, strutils, tables
+
+proc a(ids: seq[string]): int =
+  var
+    t2 = 0
+    t3 = 0
+  for s in ids:
+    var
+      d2 = 0
+      d3 = 0
+    for k, v in toCountTable(s):
+      if v == 2:
+        d2 = 1
+      if v == 3:
+        d3 = 1
+    t2 += d2
+    t3 += d3
+  return t2 * t3
+
+proc b(ids: seq[string]): string =
+  var seen = initSet[string]()
+  for s in ids:
+    for c in 0..len(s):
+      let k = format("%s_%s", s[0..c], s[c+1..^1])
+      if k in seen:
+        return s[0..c] & s[c+1..^1]
+      seen.incl(k)
+
+let ids = readFile("input").splitWhitespace
+echo "A: ", a(ids)
+echo "B: ", b(ids)
