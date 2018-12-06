bac0aefb4bbcc7e505d892737b8d55f3a30330aa
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-12-06 06:57:18 +0200

    python template simplifies one item per line

--- template/main.py
+++ template/main.py
@@ -9,19 +9,21 @@ import json
 import re
 from collections import defaultdict, deque, namedtuple
 
-lines = [l.strip().split() for l in open('input') if l.strip()]
-try:
-    nums = [[int(x) for x in l] for l in lines]
-    lines = nums
-except:
-    pass
-
 def solveA(lines):
     for line in lines:
         print(line)
 
 def solveB(lines):
     pass
+
+lines = [l.strip().split() for l in open('input') if l.strip()]
+try:
+    nums = [[int(x) for x in l] for l in lines]
+    lines = nums
+except:
+    pass
+if all(len(l) == 1 for l in lines):
+    lines = [l[0] for l in lines]
 
 print('A', solveA(lines))
 print('B', solveB(lines))
