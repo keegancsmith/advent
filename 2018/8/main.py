cf7d43769043d3753ab887fea9b847f77ef4dd65
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-12-08 07:22:29 +0200

    8: Cleaned up python solution

--- 2018/8/main.py
+++ 2018/8/main.py
@@ -1,53 +1,37 @@
 #!/usr/bin/env python3
 
-import bisect
-import fileinput
-import hashlib
-import heapq
-import itertools
-import json
-import re
-from collections import defaultdict, deque, namedtuple
-
 def parse_node(nums):
     n_child = nums[0]
     n_metadata = nums[1]
     nums = nums[2:]
-    sums = [0]
-    ans = 0
+    children = []
     for i in range(n_child):
-        ms, nums = parse_node(nums)
-        sums.append(ms)
+        child, nums = parse_node(nums)
+        children.append(child)
     metadata = nums[:n_metadata]
     nums = nums[n_metadata:]
+    return (children, metadata), nums
+
+def solveA(node):
+    children, metadata = node
+    return sum(metadata) + sum(solveA(n) for n in children)
+
+def solveB(node):
+    children, metadata = node
+    if len(children) == 0:
+        return sum(metadata)
+    sums = [0] + [solveB(n) for n in children]
+    ans = 0
     for i in metadata:
         if i < len(sums):
            ans += sums[i]
-    if len(sums) == 1:
-        ans = sum(metadata)
-    return ans, nums
+    return ans
 
-def solveA(lines):
-    nums = lines
-    metadata_sum = 0
-    while nums:
-        ms, nums = parse_node(nums)
-        metadata_sum += ms
-    return metadata_sum
+nums = [int(x) for x in open('input').read().split()]
+#nums = list(map(int, '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'.split()))
 
-def solveB(lines):
-    pass
+root, nums = parse_node(nums)
+assert len(nums) == 0
 
-lines = [l.strip().split() for l in open('input') if l.strip()]
-try:
-    nums = [[int(x) for x in l] for l in lines]
-    lines = nums
-except:
-    pass
-if all(len(l) == 1 for l in lines):
-    lines = [l[0] for l in lines]
-
-lines = lines[0]
-#lines = list(map(int, '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'.split()))
-    
-print('A', solveA(lines))
+print('A', solveA(root))
+print('B', solveB(root))
